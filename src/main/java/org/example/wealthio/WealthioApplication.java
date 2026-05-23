233f1ec346afda3c94759f8b2c397d51fb37651a
RishitModi <modirishit6@gmail.com>
2026-05-23 10:25:24 +0530

    Initial commit

---
+++ src/main/java/org/example/wealthio/WealthioApplication.java
@@ -0,0 +1,13 @@
+package org.example.wealthio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WealthioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WealthioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/wealthio/WealthioApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.wealthio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WealthioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
