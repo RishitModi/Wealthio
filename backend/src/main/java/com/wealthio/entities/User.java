2189a9a062056a639d6138a1b07f61fde2600696
anshiii14 <anshigangar@gmail.com>
2026-07-04 22:59:52 +0530

    hopefully its fixed

--- backend/src/main/java/com/wealthio/entities/User.java
+++ backend/src/main/java/com/wealthio/entities/User.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "users")
+@Table(name = "users", schema = "public")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
